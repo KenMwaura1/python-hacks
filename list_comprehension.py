373a40854ad86ee5e69f7e046e667feb814cb1d3
KenMwaura1 <kemwaura@gmail.com>
2020-08-15 15:06:32 +0300

    added a script for list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,5 @@
+""" using single line list comprehension """
+
+list1 = [6, 7, 8, 9, 10, 11]
+list2 = [2 * e for e in list1]  # constructing a list from a new list from the previous one
+print(list2)
